fd3bb1a035ee06a19df7c6040ece31a3b07b9a87
MrHelloWorldHelloKitty <benson9312@gmail.com>
2025-02-15 18:09:30 -0800

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Login from "./Login";
 import Dashboard from "./dashboard";
 
-function App(){
-  return(
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="dashboard" element={<Dashboard/>}/>
-        </Routes>
-      </BrowserRouter>
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
   );
 }
 
