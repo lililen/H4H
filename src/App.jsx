20ad9001f0675fd8e37505b412f3a3d6010149dd
MrHelloWorldHelloKitty <benson9312@gmail.com>
2025-02-15 19:28:35 -0800

    dashboard ui edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login";
 import Dashboard from "./dashboard";
 
-
 const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setUser={setUser} />} />
+        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
