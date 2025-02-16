25419e0c5e97f4ffdb59d58620c545a709893de0
MrHelloWorldHelloKitty <benson9312@gmail.com>
2025-02-15 21:25:31 -0800

    2125

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Login from "./Login";
-import Dashboard from "./dashboard";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login";
+import Signup from "./signup";
+import Dashboard from "./Dashboard";
 
-function App(){
-  return(
-    <BrowserRouter>
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
       <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="dashboard" element={<Dashboard/>}/>
-        </Routes>
-      </BrowserRouter>
+        {/* Login Page */}
+        <Route path="/" element={<Login setUser={setUser} />} />
+
+        {/* Sign-Up Page */}
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Dashboard (Redirects if not logged in) */}
+        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
-/* .*/
